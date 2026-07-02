964be3bc57ea13dfb9b380f45be4339d1030b121
gauravjain6065-maker <gauravjain6065@gmail.com>
2026-07-02 19:27:39 +0530

    More Modified

--- ai_crm/www/lead-management/index.py
+++ ai_crm/www/lead-management/index.py
@@ -3,8 +3,7 @@
 # def get_context(context):
 #     context.no_cache = 1
 import frappe
-
-   def get_context(context):
-       if frappe.session.user == "Guest":
-           frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
-           raise frappe.Redirect
+def get_context(context):
+      if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
+        raise frappe.Redirect
--- ai_crm/www/lead-source/index.py
+++ ai_crm/www/lead-source/index.py
@@ -5,8 +5,7 @@
 #            frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
 #            raise frappe.Redirect
 import frappe
-
-   def get_context(context):
-       if frappe.session.user == "Guest":
-           frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
-           raise frappe.Redirect
+def get_context(context):
+      if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
+        raise frappe.Redirect
