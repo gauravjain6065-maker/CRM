ec7830971cb900ece84bb0f282676f2b3e1dda2d
gauravjain6065-maker <gauravjain6065@gmail.com>
2026-07-02 19:37:44 +0530

    udate

--- ai_crm/www/area-management/index.py
+++ ai_crm/www/area-management/index.py
@@ -4,7 +4,7 @@ template = "templates/pages/area-management.html"
 #     context.no_cache = 1
 import frappe
 
-   def get_context(context):
-       if frappe.session.user == "Guest":
-           frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
-           raise frappe.Redirect
+def get_context(context):
+    if frappe.session.user == "Guest":
+        frappe.local.flags.redirect_location = "/login?redirect-to=/area-management"
+        raise frappe.Redirect
