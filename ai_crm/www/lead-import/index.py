9b5eee1f07b3e9571529a1127ceb5289f595b4af
Raj Sonar <rajsonar083@gmail.com>
2026-07-04 16:37:21 +0000

    WIP: save local changes

--- ai_crm/www/lead-import/index.py
+++ ai_crm/www/lead-import/index.py
@@ -1,4 +1,13 @@
+import frappe
+
 template = "templates/pages/lead-import.html"
 
 def get_context(context):
     context.no_cache = 1
+    
+    # Restrict to CRM Admin / Administrator
+    user = frappe.session.user
+    roles = frappe.get_roles(user)
+    if "CRM Admin" not in roles and user != "Administrator":
+        frappe.local.flags.redirect_location = "/lead-management"
+        raise frappe.Redirect
